5656a29d2caa4aed47e193c67971e38aec954fd6
icybaker <jrwiens0707@gmail.com>
2018-01-28 18:05:43 -0600

    Beginning display testing

--- script/nav-bar.js
+++ script/nav-bar.js
@@ -1,6 +1,8 @@
 class NavBar {
     constructor(barContainerSelector){
+        
         this.barContainer = document.querySelectorAll(barContainerSelector);
+        console.log(this.barContainer);
     }
-    
+
 }
