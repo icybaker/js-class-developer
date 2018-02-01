8df95d770211fb58e9455f85bcbc3ec6337e972c
unknown <jrwiens0707@gmail.com>
2018-02-01 12:14:11 -0600

    NavBar ready for desktop use. Next time, mobile update

--- script/main.js
+++ script/main.js
@@ -1,12 +1,46 @@
 function setAttrs(el, attrValArray){
-    for(var i=0;i<attrValArray.length;i++){
-        el.setAttribute(attrValArray[i][0],attrValArray[i][1]);
+        for(var i=0;i<attrValArray.length;i++){
+            el.setAttribute(attrValArray[i][0],attrValArray[i][1]);
+        }
+}
+function setAttrs1D(elArray, attrValArray){//note that each element in elArray will each recieve the same set of attributes
+    for(var j=0;j<elArray.length;j++){
+        for(var i=0;i<attrValArray.length;i++){
+            elArray[j].setAttribute(attrValArray[i][0],attrValArray[i][1]);
+        }
+    }
+}
+function setAttrs2D(elArray, attrValArray){//note that each element in elArray will each recieve the same set of attributes
+    for(var k=0;k<elArray[0].length;k++){    
+        for(var j=0;j<elArray.length;j++){
+            for(var i=0;i<attrValArray.length;i++){
+                elArray[k][j].setAttribute(attrValArray[i][0],attrValArray[i][1]);
+            }
+        }
     }
 }
 
+
 function setStyles(el,propValArray){
+    //console.log(propValArray);
     for(var i=0;i<propValArray.length;i++){
         el.style[propValArray[i][0]] = propValArray[i][1];
+    }
+}
+function setAttrs1D(elArray, propValArray){//note that each element in elArray will each recieve the same set of attributes
+    for(var j=0;j<elArray.length;j++){
+        for(var i=0;i<attrValArray.length;i++){
+            elArray[j].style[propValArray[i][0]] = propValArray[i][1];
+        }
+    }
+}
+function setAttrs2D(elArray, propValArray){//note that each element in elArray will each recieve the same set of attributes
+    for(var k=0;k<elArray[0].length;k++){
+        for(var j=0;j<elArray.length;j++){
+            for(var i=0;i<attrValArray.length;i++){
+                elArray[k][j].style[propValArray[i][0]] = propValArray[i][1];
+            }
+        }   
     }
 }
 
--- script/nav-bar.js
+++ script/nav-bar.js
@@ -1,48 +1,40 @@
 class NavBar {
-    constructor(barContainerSelector,baseColors = ["#fff","#000"]){
+    constructor(barContainerSelector,{itemBackgroundColor = '#fff', textColor = '#000', fontSize = '100%'}={}){
+        this.mediaToggle = this._setMediaToggle();
         this.bar = document.querySelectorAll(barContainerSelector)[0];//1 div
         this.menus = Array.from(this.bar.children);//array of divs
         this.menuLabels = this._getMenuLabels(this.menus);//array of divs
         this.menuLists = this._getMenuLists(this.menuLabels);//array of divs
         this.menuItems = this._getMenuItems(this.menuLists);//array of array (2D) of divs
         this.itemData = this._getItemData(this.menuItems);//3D array of the same cross-section as menuItems, but with each cell storing an array of form [(str)menu-item-text,(str)menu-item-page-src]
-        //console.log(this.itemData);
-        this.baseColors = baseColors;
-        //console.log(this.menuLists[0]);
+        this.styleData = this._getStyleData(itemBackgroundColor,textColor,fontSize,this.mediaToggle);//object whose properties are 2D arrays of style data for each class in the NavBar structure
+        /* {bg:['backgroundColor',itemBackgroundColor],txtC:['color',textColor],fS:['fontSize',fontSize]}; */
+
+        /*for(var prop in arguments[1]){
+            console.log(typeof prop);
+        }*/
+        
+        this._setStyles(this.styleData,this.bar,this.menus,this.menuLabels,this.menuLists,this.menuItems);
         this._setPageLinks(this.itemData,this.menuItems);
         this._attachListeners(this.menus,this.menuLabels, this.menuItems);
-        
-        this._setBarHeight(this.menuLabels[0],this.bar);
-        //this.barHeight = this.menus[0].children[0].clientHeight;
-
-        //this.bar.style.height = this.barHeight + "px";
-
-        // console.log(this.menus[0].children[0].nextSibling);
-    }
-
-    _setBarHeight(ref,targ){
-        console.log(ref.clientHeight);
-        targ.style.height = ref.clientHeight;
-        console.log(targ.style.height);
     }
 
     _attachListeners(menus,labels,items){
-        this._setColors(labels,items);
         for(var i=0;i<labels.length;i++){
-            labels[i].addEventListener("mouseenter",this._ev_showList,false);
-            menus[i].addEventListener("mouseleave",this._ev_hideList,false);
+            labels[i].addEventListener('mouseenter',this._ev_showList,false);
+            menus[i].addEventListener('mouseleave',this._ev_hideList,false);
         }
        for(i=0;i<items.length;i++){
             for(var j=0;j<items[0].length;j++){
-                items[i][j].addEventListener("mouseenter",this._ev_invertTxt,false);
-                items[i][j].addEventListener("mouseleave",this._ev_invertTxt,false);
+                items[i][j].addEventListener('mouseenter',this._ev_invertTxt,false);
+                items[i][j].addEventListener('mouseleave',this._ev_invertTxt,false);
             }
         }
 
     }
 
     _ev_invertTxt(evt){
-        console.log(evt.target.style.color);
+        //console.log(evt.target.style.color);
         var targ = evt.target, bgOld = targ.style.backgroundColor,
         cOld = targ.style.color;
         targ.style.backgroundColor = cOld;
@@ -51,43 +43,65 @@ class NavBar {
 
     _ev_showList(evt){
         var list = evt.target.nextElementSibling;
-        list.style.display = "block";
-        console.log(evt.target);
+        list.style.display = 'block';
+        //console.log(evt.target);
     }
     _ev_hideList(evt){
-        console.log(evt.target);
+        //console.log(evt.target);
         var list = evt.target.children[1];
-        list.style.display = "none";
+        list.style.display = 'none';
         
     }
 
-    _setColors(labels,items){
-        for(var i=0;i<labels.length;i++){
-            labels[i].style.backgroundColor = "transparent";
-            labels[i].style.color = this.baseColors[1];
-        }
-       for(i=0;i<items.length;i++){
-            for(var j=0;j<items[0].length;j++){
-                items[i][j].style.backgroundColor = this.baseColors[0];
-                items[i][j].style.color = this.baseColors[1];
+    _setPageLinks(data,items){
+        for(var i=0;i<data.length;i++){
+            var jD = new Array(data[0].length);
+            for(var j=0;j<data[0].length;j++){
+                jD[j] =  document.createElement('a');
+                jD[j].setAttribute('href',data[i][j][1]);
+                jD[j].innerHTML = data[i][j][0];
+                jD[j].style.color = 'inherit';
+                //console.log(jD[j]);
+                items[i][j].innerHTML = '';
+                items[i][j].appendChild(jD[j]);
             }
         }
     }
 
-    _setPageLinks(data,items){
+    _setStyles(data,bar,menus,labels,lists,items){
+        console.log(menus.length);
+        //this.bar.style.fontSize = FS;
 
-        for(var i=0;i<data.length;i++){
-            var jD = new Array(data[0].length);
-            for(var j=0;j<data[0].length;j++){
-                jD[j] =  document.createElement("a");
-                jD[j].setAttribute("href",data[i][j][1]);
-                jD[j].innerHTML = data[i][j][0];
-                jD[j].style.color = "inherit";
-                console.log(jD[j]);
-                items[i][j].innerHTML = "";
-                items[i][j].appendChild(jD[j]);
+        setStyles(bar,data.bar);
+        for(var i=0;i<menus.length;i++){
+            setStyles(menus[i],data.menus);
+            if(menus[i].classList.contains('right')){menus[i].style.float = 'right';}
+            setStyles(labels[i],data.labels);
+            setStyles(lists[i],data.lists);
+            for(var j=0;j<items[0].length;j++){
+                
+                setStyles(items[i][j],data.items);
             }
         }
+    }
+
+    _getStyleData(bgC,txtC,fS,tog){
+        var data = {}, barHeight = this.menuLabels[0].clientHeight;
+        if(tog){//desktop
+            data.bar = [['height',barHeight+'px'],['fontSize',fS]];
+            data.menus = [['display','inline-block'],['verticalAlign','top']];
+            data.labels = [['color',txtC],['cursor','default']];
+            data.lists = [['display','none']];
+            data.items = [['backgroundColor',bgC],['color',txtC]];
+        }
+        else{//Mobile
+            data.bar = [['fontSize',fS]];
+            data.menus = [['display','block']];
+            data.labels = [['color',txtC],['cursor','default']];
+            data.lists = [['display','none']];
+            data.items = [['backgroundColor',bgC],['color',txtC]];
+        }
+        return data;
     }
 
     _getItemData(items){
@@ -97,7 +111,7 @@ class NavBar {
         for(var i=0;i<items.length;i++){
             var jD = new Array(items[0].length);
             for(var j=0;j<items[0].length;j++){
-                jD[j] = items[i][j].innerHTML.split(",");
+                jD[j] = items[i][j].innerHTML.split(',');
                 //console.log(jD);
             }
             iD[i] = jD;
@@ -130,8 +144,14 @@ class NavBar {
         return labels;
     }
 
+    _setMediaToggle(){
+        var w = window.innerWidth, h = window.innerHeight;
+        if((w/h)>1){return 1;}
+        else{return 0;}
+    }
+
     static _doc_(){
-        var doc = "Arguments:\nbarContainerSelecctor = (string) #id.nav-bar\nbaseColors = [(string) style.backgroundColor, (string) style.color]\n\nUsage:\nMust construct the NavBar foundation in the HTML page\nOnly the first level of the hierarchy needs identification\nIt requires a class of (no quotes) 'nav-bar' and an id of your choice\nThe structure is created as a hierarchy of divs.\nThe model is as follows:\n<div class='nav-bar' id='you pick'>\n   <div>Menu container, can be many of these per bar\n       <div>text that will display on menu labels</div>1 of these per menu\n       <div>container for menu item list (the part that is invisible unless moused over); 1 per menu\n           <div>**this is a , delimited list**Text that will display on menu items,link source to page</div> many of these per item list container\n           .\n           .\n           .\n       </div>\n   </div>\n   .\n   .\n   .\n</div>\n\nThe baseColors argument defaults to white background and black letters\nIt sets these colors in the menu items and labels except that the label's background is transparent.\nAs well, menu items swap these two colors on mouseover.\nThe nav-bar element's color should be set on an individual basis as it is what is constantly visible on the page."
+        var doc = "Arguments:\nbarContainerSelecctor = (str) #id.nav-bar\n{itemBackgroundColor : (str) <color value>, textColor : (str) <color value>, fontSize : (str) <size value>}\nWhere everything in {} is optional* \nitemBackgroundColor - effects the individual items within the menu; can be any valid CSS. \ntextColor - effects both the items and the menu label. \nfontSize - effects all text within the NavBar object.\n*Note: Should you choose to include none of the optional items, no {} is necessary at all, they will default to '#fff','#000','100%', respectively. \n\nExample:\n\n   var navBar = new NavBar('#main.nav-bar,{itemBackgroundColor: 'white', textColor: 'black', fontSize: '1em'});\n\nThis will initiate the NavBar taking care of all CSS and event handling requirements. \nThis line of code belongs at the bottom of you <body> element inside a <script></script> block.\nNote: you can add your own classes and CSS, and the NavBar class \nwill overwrite only the things it needs to without effecting other user specified CSS. \n\nHTML Prerequisite:\nMust construct the HTML foundation for the NavBar class to inhabit via a hierarchy of divs. \nOnly the first level div of the hierarchy needs identification for the NavBar to function. \nIt can be any CSS selector as long as a query for that selector returns only 1 element. \n\nAs mentioned earlier, the structure is created as a hierarchy of divs.\nThe model is as follows: \n\n<div class='Some Class' id='someID'>\n   <div>Menu container, can be many of these per bar\n       <div>text that will display on menu labels</div>1 of these per menu\n       <div>container for menu item list (the part that is invisible unless moused over); 1 per menu\n           <div>**this is a comma delimited list**Text that will display on menu items,link source to page</div> many of these per item list container\n           .\n           .\n           .\n       </div>\n   </div>\n   .\n   .\n   .\n</div> \n\n**Notice the contents of the innermost div.\nYou can think of these elements sort of like functions, in that \nwhat you put inside these divs will be used as input to the NavBar class. \nDon't worry though, it's just comma delimited plain text.\nThe first 'argument' is just the item label exactly as you want it to appear \nand the second is the relative filepath to the page. \n\nExtras: \nSimilarly to the note above, you can also pass data/options to the NavBar \nby giving special classes to certain div elements. \nAs of now, there is only one such class which is 'right', and can be applied to \nmenu container elements (2nd level divs) to float that menu to the right side of the NavBar."
         console.log(doc);
         return doc;
     }
